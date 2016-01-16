a819749c9dc7604848ea6683bf99a30172296909
Arcade-Renegade <chris@arcade-renegade.com>
2016-01-15 18:31:30 -0800

    Added some code to Hello World
    Added some code to Hello World

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,12 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World.");
+		String name = "Chris";
+		int age = 26;
+		String passion = "Video game development";
+		
+		System.out.println("My name is " + name + ".");
+		System.out.println("I am " + age + " years old.");
+		System.out.println("I chose Computer Science because my passion in life is " + passion + ".");
 	}
 }
