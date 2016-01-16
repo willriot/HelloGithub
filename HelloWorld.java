4738e5879ed0a42a6eaaaad58d2745bfefbb451c
Arcade-Renegade <chris@arcade-renegade.com>
2016-01-15 18:34:31 -0800

    Refactored a variable
    passion -> lifePassion

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,10 +5,10 @@ public class HelloWorld {
 		System.out.println("Hello World.");
 		String name = "Chris";
 		int age = 26;
-		String passion = "Video game development";
+		String lifePassion = "Video game development";
 		
 		System.out.println("My name is " + name + ".");
 		System.out.println("I am " + age + " years old.");
-		System.out.println("I chose Computer Science because my passion in life is " + passion + ".");
+		System.out.println("I chose Computer Science because my passion in life is " + lifePassion + ".");
 	}
 }
